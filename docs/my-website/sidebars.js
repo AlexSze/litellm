be7b0ff31b7faa67582b19edcacafe037ccd9b87
Maruti Agarwal <88403147+marutilai@users.noreply.github.com>
2026-04-24 01:16:45 -0400

    feat(ocr): add Reducto parse OCR support (#26068)
    * feat(ocr): add Reducto parse OCR support
    
    * fix(reducto): address OCR review feedback
    
    * chore: refresh uv lockfile
    
    * Revert "chore: refresh uv lockfile"
    
    This reverts commit 47200c0e603275108335aee852d0a96586165337.

--- docs/my-website/sidebars.js
+++ docs/my-website/sidebars.js
@@ -995,6 +995,7 @@ const sidebars = {
         "providers/predibase",
         "providers/pydantic_ai_agent",
         "providers/ragflow",
+        "providers/reducto",
         "providers/recraft",
         "providers/replicate",
         {
